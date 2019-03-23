3e5fa906a9cd2fb0ed794e60c319f51c6b2bca27
Camilo <camilo.agudelo4368@gmail.com>
2019-03-23 02:53:43 -0400

    second commit movie ready

--- liri.js
+++ liri.js
@@ -15,12 +15,30 @@ var action = process.argv[2];
 var value = process.argv[3];
 var artistsList = [];
 
-// spotify-this-song
-if (action === "spotify-this-song") {
-    // console.log(value)
+// concert-this
+if (action === "concert-this") {
     searchSpotify(value);
 }
 
+// spotify-this-song
+if (action === "spotify-this-song") {
+    searchSpotify(value);
+}
+
+// movie-this
+if (action === "movie-this") {
+    searchMovie(value);
+}
+
+
+/**
+ *Serach for concert events
+ *
+ * @param {*} _value event to search
+ */
+function searchConcert(_value){
+
+}
 
 /**
  *Serch tracks on spotify
@@ -45,7 +63,7 @@ function searchSpotify(_value) {
                 var spotifyObject = {
                     artists: artistsList,
                     name: item.name,
-                    "preview Link":item.preview_url,
+                    "preview Link": item.preview_url,
                     album: item.album.name
                 };
 
@@ -61,12 +79,40 @@ function searchSpotify(_value) {
         });
 }
 
+/**
+ *Search moive
+ *
+ * @param {*} _value name of movie
+ */
+function searchMovie(_value) {
+    axios.get("http://www.omdbapi.com/?t=" + _value.split(' ').join('+') + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            var rottenRating = 'NULL';
+
+            response.data.Ratings.forEach(element => {
+                if (element.Source === 'Rotten Tomatoes') {
+                    console.log(element)
+                    rottenRating = element.Value;
+                }
+            })
+
+            // console.log(response.data)
+            var movieObject = {
+                Title: response.data.Title,
+                "Released": response.data.Released,
+                Rating: response.data.imdbRating,
+                "Rotten Tomatoes Rating": rottenRating,
+                Country: response.data.Country,
+                Language: response.data.Language,
+                Plot: response.data.PLot,
+                Actors: response.data.Actors
+            }
+
+            return console.log(JSON.stringify(movieObject, null, 2));
+        }
+    );
+}
 
 
-// concert-this
-
-
-
-// movie-this
 
 // do-what-it-says
