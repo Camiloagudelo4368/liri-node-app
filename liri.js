811f2cae932b3143da34daf1a112deb1709084e3
Camilo <camilo.agudelo4368@gmail.com>
2019-03-23 13:39:00 -0400

    finfish funcionality

--- liri.js
+++ liri.js
@@ -5,29 +5,55 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var fs = require("fs");
+var moment = require('moment');
+var divider  = "\n ///-----------------------------------------------------------------------------------------------///" + '\n'
 var Spotify = require('node-spotify-api');
-
-// Access your keys information like so
 var spotifyNew = new Spotify(keys.spotify);
-
-
 var action = process.argv[2];
-var value = process.argv[3];
+var value = process.argv.splice(3).join('+');
 var artistsList = [];
 
-// concert-this
-if (action === "concert-this") {
-    searchSpotify(value);
+
+/**
+ *Main liri search action
+ *
+ * @param {*} _action type of action Could be "concert-this", "spotify-this-song", "movie-this" or "do-what-it-says"
+ * @param {*} _value value to be consulted
+ */
+function searchLiri(_action, _value) {
+    // concert-this
+    if (_action === "concert-this") {
+        searchConcert(_value);
+    }
+
+    // spotify-this-song
+    if (_action === "spotify-this-song") {
+        searchSpotify(_value);
+    }
+
+    // movie-this
+    if (_action === "movie-this") {
+        searchMovie(_value);
+    }
+
+    if (_action === "do-what-it-says") {
+        searchRandomFile();
+    }
+
 }
 
-// spotify-this-song
-if (action === "spotify-this-song") {
-    searchSpotify(value);
-}
 
-// movie-this
-if (action === "movie-this") {
-    searchMovie(value);
+/**
+ *Log results on log.txt file
+ *
+ * @param {*} text
+ */
+function logAction(text){
+    fs.appendFile("Log.txt", text + divider,  error =>{
+        if (error) {
+            throw error;
+        }        
+    })
 }
 
 
@@ -36,8 +62,26 @@ if (action === "movie-this") {
  *
  * @param {*} _value event to search
  */
-function searchConcert(_value){
+function searchConcert(_value) {
+    axios.get("https://rest.bandsintown.com/artists/" + _value + "/events?app_id=codingbootcam").then(
+        function (response) {
+            var data = response.data;
 
+            var concertList = [];
+
+            data.forEach(element => {
+                var concertObject = {
+                    Name: element.venue.name,
+                    Venue: element.venue.city + ', ' + element.venue.country,
+                    Date: moment(element.datetime).format("MM/DD/YYYY")
+                }
+
+                concertList.push(concertObject);
+            })
+
+            logAction(JSON.stringify(concertList, null, 2));
+            console.log(JSON.stringify(concertList, null, 2))
+        })
 }
 
 /**
@@ -67,7 +111,8 @@ function searchSpotify(_value) {
                     album: item.album.name
                 };
 
-                return console.log(JSON.stringify(spotifyObject, null, 2));
+                logAction(JSON.stringify(spotifyObject, null, 2));
+                console.log(JSON.stringify(spotifyObject, null, 2));
 
             }
             else {
@@ -85,7 +130,7 @@ function searchSpotify(_value) {
  * @param {*} _value name of movie
  */
 function searchMovie(_value) {
-    axios.get("http://www.omdbapi.com/?t=" + _value.split(' ').join('+') + "&y=&plot=short&apikey=trilogy").then(
+    axios.get("http://www.omdbapi.com/?t=" + _value + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             var rottenRating = 'NULL';
 
@@ -104,15 +149,42 @@ function searchMovie(_value) {
                 "Rotten Tomatoes Rating": rottenRating,
                 Country: response.data.Country,
                 Language: response.data.Language,
-                Plot: response.data.PLot,
+                Plot: response.data.Plot,
                 Actors: response.data.Actors
             }
 
-            return console.log(JSON.stringify(movieObject, null, 2));
+            logAction(JSON.stringify(movieObject, null, 2));
+            console.log(JSON.stringify(movieObject, null, 2));
         }
     );
 }
 
 
+/**
+ *Read the parameters on random file and console log
+ *
+ */
+function searchRandomFile() {
+
+    fs.readFile("random.txt", 'utf8' , function(error, text) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
+        }
+        
+        // console.log(text);
+        var data = text.split(",");
+
+         console.log(data[0])
+         console.log(data[1])
+
+        // Call main search function
+        searchLiri(data[0], data[1]);
+
+    })
+}
+
+searchLiri(action, value);
 
 // do-what-it-says
